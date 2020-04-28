bcbc4f29a3fb0faa8d04c4c13ae9bf5636042766
Richard Gilmour <rgilmour.mail@gmail.com>
2020-04-28 08:55:45 +0100

    Added panel for different sum types

--- src/SumView/SumView.jsx
+++ src/SumView/SumView.jsx
@@ -2,13 +2,24 @@ import React, { useState, useEffect } from 'react';
 import DisplayBox from '../DisplayBox';
 import InputBox from '../InputBox';
 import SubmitButton from '../SubmitButton'
+import SumOptionsPanel from '../SumOptionsPanel';
 
 import './SumView.scss';
-import { getSimpleAddition, getSimpleMultiplication } from '../utils/sums.js';
+import { getSimpleAddition, getSimpleMultiplication, getSimpleDivision, getSimpleSubtraction } from '../utils/sums.js';
 
 const problemTypes = {
     addition: getSimpleAddition,
     multiplication: getSimpleMultiplication,
+    division: getSimpleDivision,
+    subtraction: getSimpleSubtraction,
+};
+
+// TODO, move
+const symbolsProblemMap = {
+    ADDITION: { symbol: "+", problemType: "addition" },
+    MULTIPLICATON: { symbol: "×", problemType: "multiplication" },
+    DIVISION: { symbol: "÷", problemType: "division" },
+    SUBTRACTION: { symbol: "−", problemType: "subtraction" },
 };
 
 const SumView = () => {
@@ -17,20 +28,27 @@ const SumView = () => {
     const [problem, setProblem] = useState('addition');
     const [sum, setSum] = useState(getSimpleAddition());
 
-    const onSubmitHandler = () => setResult(value === sum.right ? "Correct" : "Wrong");
+    const onSubmitHandler = () => {
+        if (result === "Correct") {
+            // TODO hacky. if already correctly submitted once
+            onNewSum();
+        } else {
+            setResult(value === sum.right ? "Correct" : "Wrong");
+        }
+    };
 
     const getProblemFunction = () => (problemTypes[problem]);
-    
+
     const onNewSum = () => setSum(getProblemFunction.call());
 
-    const onSumTypeChange = () => setProblem(problem === 'addition' ? 'multiplication' : 'addition');
+    const onSumTypeChange = (problemType) => setProblem(problemType);
 
     const onInputValueChange = (newIntValue) => setValue(newIntValue);
 
-    useEffect(() => { 
+    useEffect(() => {
         setSum(problemTypes[problem].call())
-     }, [problem]);
-    
+    }, [problem]);
+
     useEffect(() => {
         setValue(null);
         setResult(null);
@@ -46,12 +64,12 @@ const SumView = () => {
                 <span className="SumView-sign">=</span>
                 <DisplayBox value={sum.answer} />
             </div>
-            <div>
                 <SubmitButton onClickHandler={onSubmitHandler} />
+            <div>
                 <h3>Debug info: {result}</h3>
                 <button onClick={onNewSum}>New sum</button>
-                <button onClick={onSumTypeChange}>Change</button>
             </div>
+            <SumOptionsPanel onClickHandler={onSumTypeChange} problemType={problem} problemMap={Object.values(symbolsProblemMap)} />
         </>
     )
 };
