5a111a99599fdf075f6b5786b3b26ec5b23d319b
Richard Gilmour <rgilmour.mail@gmail.com>
2020-05-01 23:35:22 +0100

    Updates

---
+++ precache-manifest.bbc269ea63887744ed39e1853d31be76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e593695fe4290ccab0df3fd48dfa5375",
+    "url": "/kids-maths-app/index.html"
+  },
+  {
+    "revision": "dc10c284b014bc83fd1b",
+    "url": "/kids-maths-app/static/css/main.8e03addc.chunk.css"
+  },
+  {
+    "revision": "c7850def32d600bca034",
+    "url": "/kids-maths-app/static/js/2.6637deae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kids-maths-app/static/js/2.6637deae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc10c284b014bc83fd1b",
+    "url": "/kids-maths-app/static/js/main.b4f9ee88.chunk.js"
+  },
+  {
+    "revision": "5e79d7f93c51b450d68f",
+    "url": "/kids-maths-app/static/js/runtime-main.95b93fe0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1a697c41d54ff84a6201ee2e16e25900.js"
+  "/kids-maths-app/precache-manifest.bbc269ea63887744ed39e1853d31be76.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kids-maths-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
