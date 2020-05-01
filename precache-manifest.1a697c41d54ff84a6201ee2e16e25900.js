4328f0cb52cf8d50eae7abbebf1a7af9be7ec26a
Richard Gilmour <rgilmour.mail@gmail.com>
2020-05-01 23:18:21 +0100

    Updates

---
+++ precache-manifest.1a697c41d54ff84a6201ee2e16e25900.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d5e36392aa5882c8f4099f901f714e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "166fb4869cb3ddb5df08",
+    "url": "/static/css/main.8e03addc.chunk.css"
+  },
+  {
+    "revision": "c7850def32d600bca034",
+    "url": "/static/js/2.6637deae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6637deae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "166fb4869cb3ddb5df08",
+    "url": "/static/js/main.b79f38c0.chunk.js"
+  },
+  {
+    "revision": "55a5ee051ccf59240d9d",
+    "url": "/static/js/runtime-main.846c24bd.js"
+  }
+]);
